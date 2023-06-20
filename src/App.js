a9a55e9d7c7c8bc386885823e1491fa42a518448
sagayaraj sounderrajan <sagayaraj8939@gmail.com>
2023-06-20 11:45:19 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import pattern from "./components/pattern";
+import React, { useState } from "react"; 
 
-function App() {
+const App = () => {
+  const init = ['','','','','','','','',''];
+  const [board, setBoard] = useState(init);
+  const [player, setPlayer] = useState("x");
+  const [count, setCount] = useState(0);
+  const [intext, setIntext] = useState("Click any of the box and start the game(TicTacToe) player1 : x");
+  const [endGame, setEndGame] = useState(false)
+  
+  const handleClick = (i) => {
+    let co = 0;
+    // console.log(i);
+    if (board[i] !== '') return;
+    if (endGame) return;
+    setCount(count+1);
+    // console.log(count);
+    board[i] = player;
+    // console.log(i);
+    setBoard(board);
+    // console.log(board);
+    setPlayer(player === 'x'? 'o' : 'x');
+    pattern.forEach((arr) => {
+      // const player1 = (player === 'x')? 'o' : 'x';
+      // console.log(player1);
+      if (board[arr[0]] === player && 
+        board[arr[1]] === player && 
+        board[arr[2]] === player){
+          co+=1;
+          setIntext("Player '" + player + "' won the game.")
+          // reset();
+          setEndGame(true);
+        }
+        else {
+          if (count === 8 && pattern[7]=== arr && co === 0){
+            setIntext("Both Lose the game.");
+            setEndGame(true);
+            // reset();
+          }
+        }
+      })
+      const player1 = player === "x" ? "o" : "x";
+      if(count !== 8 && co === 0){
+        setIntext(player1 +"'s turn");
+      }
+    }
+
+        const reset = () => {
+          setBoard(init);
+          setPlayer('x');
+          setCount(0);
+          setIntext("Click any of the box and start the game(TicTacToe) player1 : x");
+          setEndGame(false);
+        }
+        
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <p className="text">{intext}</p>
+    <div className="board">
+        {board.map((val, i) => {
+          return <div key={i} className="board_tiles" onClick={() => handleClick(i)}>{val}</div>
+        })}
+    </div>
+    <button onClick={reset}>Reset</button>
     </div>
   );
 }
